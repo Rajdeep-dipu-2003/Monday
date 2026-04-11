43b009735f85dbc54cda11783df8bf47ca851446
Rajdeep-dipu-2003 <rajdeepaaron1@gmail.com>
2026-04-11 20:00:27 +0530

    Update rag data model to include llm model name

--- mondayBackend/app/models/rag.py
+++ mondayBackend/app/models/rag.py
@@ -1,7 +1,13 @@
 import uuid
-from sqlalchemy import Column, Integer, String
+import enum
+from sqlalchemy import Column, Integer, String, Enum
 from sqlalchemy.orm import relationship
 from app.db.base_class import Base
+
+class Provider(enum.Enum):
+    ollama: "Ollama"
+    openai: "OpenAi"
+    gemini: "Gemini"
 
 class RAG(Base):
     __tablename__ = "rags"
@@ -9,5 +15,8 @@ class RAG(Base):
     id = Column(Integer, primary_key=True, index=True)
     uuid = Column(String, unique=True, default=lambda: str(uuid.uuid4()))
     name = Column(String, nullable=False)
+    description = Column(String, nullable=True)
+    model = Column(String, nullable=False)
+    provider = Column(Enum(Provider), nullable=False)
 
     documents = relationship("Document", back_populates="rag", cascade="all, delete")
