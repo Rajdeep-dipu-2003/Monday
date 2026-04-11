c54cea94e28dd6e767a01c3710eb1060e225d8d2
Rajdeep-dipu-2003 <rajdeepaaron1@gmail.com>
2026-04-11 11:26:45 +0530

    Initialize project, with basic routes, config and models.

---
+++ client/mondayClient/electron.js
@@ -0,0 +1,37 @@
+import { app, BrowserWindow } from 'electron';
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    });
+
+    // const startURL = isDev
+    //     ? 'http://localhost:3000'
+    //     : `file://${path.join(__dirname, '../build/index.html')}`;
+
+    const startURL = 'http://localhost:5173/';
+
+    mainWindow.loadURL(startURL);
+
+    mainWindow.on('closed', () => (mainWindow = null));
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
