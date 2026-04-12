53e7f3bb88927855186617f3d8d5e72d22f9da68
Rajdeep-dipu-2003 <rajdeepaaron1@gmail.com>
2026-04-12 19:37:59 +0530

    Complete rag factory

--- mondayBackend/app/services/rag_service_factory.py
+++ mondayBackend/app/services/rag_service_factory.py
@@ -1,24 +1,23 @@
-from typing import Optional
+from typing import Optional, Dict
 from app.services.rag_service import RAGService
+from app.models.rag import RAG
 
 class RAGServiceFactory:
     _instance: Optional['RAGServiceFactory'] = None
-    _lock = False
+    _services: Dict[str, RAGService] = {}
 
-    def __new__(cls, *args, **kwargs):
-        if not cls._instance:
+    def __new__(cls):
+        if cls._instance is None:
             cls._instance = super(RAGServiceFactory, cls).__new__(cls)
+            cls._instance._services = {}
         return cls._instance
 
-    def __init__(self):
-        if self._initialized:
-            return
-
-        self._services = {}
-        self._initialized = True
-
-    def get_service(self, rag_id: str) -> RAGService:
-        if rag_id in self._services:
-            return self._services[rag_id]
+    def get_service(self, rag: RAG) -> RAGService:
+        rag_id = str(rag.uuid)
         
+        if rag_id not in self._services:
+            self._services[rag_id] = RAGService(rag)
 
+        return self._services[rag_id]
+
+rag_service_factory = RAGServiceFactory()
