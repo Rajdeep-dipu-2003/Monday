f18bc876c5b6127b1f4181953b961c53182f305f
Rajdeep-dipu-2003 <rajdeepaaron1@gmail.com>
2026-04-13 21:36:25 +0530

    Fix: Database path should be determined according to os

--- mondayBackend/app/db/session.py
+++ mondayBackend/app/db/session.py
@@ -1,6 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from app.core.config import settings
+from app.core.paths import DATABASE_URL
+# from app.core.config import settings
 
-engine = create_engine(settings.DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
