afc126ae469fe651a5d43a2915b6d95068147fce
Rajdeep-dipu-2003 <rajdeepaaron1@gmail.com>
2026-04-18 17:02:00 +0530

    Set up hash router

--- client/mondayClient/src/App.jsx
+++ client/mondayClient/src/App.jsx
@@ -1,11 +1,24 @@
-import { useState } from 'react'
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home"
+import Create from "./Pages/Create"
+import { Toaster } from "react-hot-toast";
+import Layout from "./Layouts/Layout";
+
 import './App.css'
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <HashRouter>
+        <Toaster position="top-right" />
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="create" element={<Create />} />
+          </Route>
+        </Routes>
+      </HashRouter>
+    </>
   )
 }
 
